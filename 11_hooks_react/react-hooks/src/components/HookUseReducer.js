800bfe288ba20072d3aa146f828c43b98ab800d9
Gabriel B <gabriel.moura2009@hotmail.com>
2023-05-04 21:21:07 -0300

    feat: state useReducer

--- 11_hooks_react/react-hooks/src/components/HookUseReducer.js
+++ 11_hooks_react/react-hooks/src/components/HookUseReducer.js
@@ -1,10 +1,47 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useState } from "react";
 
 const HookUseReducer = () => {
+  const [taskText, setTaskText] = useState("");
   // dispatch dispara uma função, não apenas atribuí o valor
   const [number, dispatch] = useReducer((state, action) => {
-    return Math.random(state);
+    return (Math.random(state) * 10).toFixed();
   });
+
+  const initialTasks = [
+    { id: 1, text: "Fazer uma coisa" },
+    { id: 2, text: "Fazer outra coisa" },
+  ];
+
+  const taskReducer = (state, action) => {
+    switch (action.type) {
+      case "ADD":
+        const newTask = {
+          id: Math.random(),
+          text: taskText,
+        };
+
+        setTaskText("");
+
+        return [...state, newTask];
+
+      case "DELETE":
+        return state.filter((task) => task.id !== action.id);
+
+      default:
+        return state;
+    }
+  };
+
+  const [tasks, dispatchTask] = useReducer(taskReducer, initialTasks);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatchTask({ type: "ADD" });
+  };
+
+  const removeTask = (id) => {
+    dispatchTask({ type: "DELETE", id: id });
+  };
 
   return (
     <div>
@@ -25,6 +62,42 @@ const HookUseReducer = () => {
 
       <p>Numero: {number}</p>
       <button onClick={dispatch}>Alterar numero</button>
+
+      <br />
+      <br />
+
+      <ul>
+        <li>
+          Se o useReducer fosse utilizado como no exemplo passado, não teria
+          tanta diferença do useState;
+        </li>
+        <li>
+          Por isso o reducer geralmente contém operações mais complexas,
+          utilizando a estrutura switch com actions
+        </li>
+        <li>Esta situação foi apresentada na seção de Context API;</li>
+      </ul>
+
+      <hr />
+
+      <h3>Tarefas</h3>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setTaskText(e.target.value)}
+          value={taskText}
+        />
+        <input type="submit" value="Enviar" />
+      </form>
+
+      <ul>
+        {tasks?.map((task) => (
+          <li key={task.id} onDoubleClick={() => removeTask(task.id)}>
+            {task.text}
+          </li>
+        ))}
+      </ul>
 
       <hr />
     </div>
